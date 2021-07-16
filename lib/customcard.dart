85c4dee49040742307b9c50550df09d2b0a7df4b
gandhisamay <gandhisamay200@gmail.com>
2021-07-16 10:33:54 +0530

    color changing effect added

--- lib/customcard.dart
+++ lib/customcard.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
   final Color colour;
-  CustomCard(this.colour);
+  final Widget child;
+  CustomCard({required this.colour, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.only(
-          left: 15,
-          right: 7.5,
-          top: 15,
-          bottom: 15,
-        ),
-        decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10),
-        ),
+    return Container(
+      margin: EdgeInsets.only(
+        left: 15,
+        right: 7.5,
+        top: 15,
+        bottom: 15,
       ),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
     );
   }
 }
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'customcard.dart';
 
 const Color colorCustomCard = Color(0xFF1D1E33);
+const Color inactiveColorCard = Color(0xFF111328);
+
+enum Gender { male, female, initial }
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,11 +12,12 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender = Gender.initial;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator'),
+        title: Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
       body: Column(
@@ -21,26 +25,91 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: [
-                CustomCard(
-                  colorCustomCard,
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: CustomCard(
+                      colour: selectedGender == Gender.initial? inactiveColorCard: selectedGender == Gender.male? colorCustomCard: inactiveColorCard,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            size: 80,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                CustomCard(
-                  colorCustomCard,
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: CustomCard(
+                      colour: selectedGender == Gender.initial? inactiveColorCard: selectedGender == Gender.female? colorCustomCard: inactiveColorCard,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.female,
+                            size: 80,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'FEMALE',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          CustomCard(
-            colorCustomCard,
+          Expanded(
+            child: GestureDetector(
+              child: CustomCard(
+                colour: colorCustomCard,
+                child: Text(''),
+              ),
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 CustomCard(
-                  colorCustomCard,
+                  colour: colorCustomCard,
+                  child: Text(''),
                 ),
                 CustomCard(
-                  colorCustomCard,
+                  colour: colorCustomCard,
+                  child: Text(''),
                 ),
               ],
             ),
